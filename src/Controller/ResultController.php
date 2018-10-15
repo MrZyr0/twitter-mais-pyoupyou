715dab38a12d99c3509c32091ddf908195cd13bb
Antinea Gontard <antinea@DESKTOP-PV0NOPD.localdomain>
2018-10-15 18:57:07 +0200

    update fixture

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResultController extends AbstractController
@@ -10,10 +12,17 @@ class ResultController extends AbstractController
     /**
      * @Route("/result", name="result")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $searchForm = $this->createForm(SearchType::class);
+        $searchForm->handleRequest($request);
+
+        $searchValue = $searchForm->getData()['searchValue'];
+        var_dump($searchValue);
         return $this->render('user/result.html.twig', [
             'controller_name' => 'ResultController',
+            'title'=>'Result',
+            'value'=>$searchValue
         ]);
     }
 }
