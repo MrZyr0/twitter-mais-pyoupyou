76717524c732103f526e9ddbb518f19f466fbc13
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-10-16 17:24:04 +0200

    Add authorizationChecker to manually dispay the right content

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage()
+    public function homepage(AuthorizationCheckerInterface $authorizationChecker)
     {
-        return $this->render('homepage.html.twig', [
-            'title' => 'Accueil',
-        ]);
+        if ($authorizationChecker->isGranted('ROLE_USER') && $authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+        {
+            return $this->render('homepage.html.twig', [
+                'title' => 'Accueil',
+            ]);
+        }
+        else
+        {
+            return $this->redirectToRoute('signin');
+        }
     }
 }
