3cc47a06192aee86d9b6d6e6dce2bb3ffe75dedb
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-10-19 09:13:51 +0200

    Update src/Form/PyoupyouType.php

--- src/Form/PyoupyouType.php
+++ src/Form/PyoupyouType.php
@@ -2,26 +2,26 @@
 
 namespace App\Form;
 
+use App\Form\Form;
 use App\Entity\Pyoupyou;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PyoupyouType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('message')
-            ->add('date')
-            ->add('isPublic')
-            ->add('user')
-            ->add('project')
-            ->add('incubator')
-            ->add('repostUsers')
-        ;
+        ->add('message', TextType::class, [
+            'label' => false,
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'Envoyer'
+        ]);
     }
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
