3db68dbc67b32dbdd274966f9683ce1a36102983
MrZyr0 <julien.seixas@ynov.com>
2018-10-19 21:01:21 +0200

    newPostType work git add .!

--- src/Controller/NewPyoupyouController.php
+++ src/Controller/NewPyoupyouController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Security\AccessChecker;
-
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Form\PyoupyouType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -18,18 +17,19 @@ use Symfony\Component\Validator\Constraints\DateTime;
 class NewPyoupyouController extends AbstractController
 {
     /**
-     * @Route("/newPyoupyou", name="new_pyou_pyou")
+     * @Route("/newPyoupyou", name="newPost")
      */
     public function index(Request $request, AccessChecker $accessChecker)
     {
-
-
         $pyoupyou = new Pyoupyou();
 
-        $form = $this->createForm(PyoupyouType::class, $pyoupyou);
+        $form = $this->createForm(PyoupyouType::class, $pyoupyou, [
+            'action' => $this->generateUrl('newPost'),
+        ]);
+
+
+
         $form->handleRequest($request);
-
-
 
         if ($form->isSubmitted() && $form->isValid())
         {
@@ -41,7 +41,9 @@ class NewPyoupyouController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($pyoupyou);
             $entityManager->flush();
-            // return $this->redirectToRoute('home');
+
+            $previousUrl = $request->server->get('HTTP_REFERER');
+            return $this->redirect($previousUrl);
         }
 
         return $this->render('layouts/form_pyoupyou.html.twig', [
